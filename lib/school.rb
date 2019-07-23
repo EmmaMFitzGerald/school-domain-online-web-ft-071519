2675b091f0a8c01d7a1a77bcfbd5733a901cd839
Emma FitzGerald <emmamariafitzgerald@gmail.com>
2019-07-23 15:46:53 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -10,8 +10,10 @@ class School
   end
   
   def add_student(name, grade)
-    @roster[grade] = []
-    @roster[grade] << name
+    if !(@roster.include?(grade))
+      @roster[grade] = []
+      @roster[grade] << name
+    end
   end
   
 end
