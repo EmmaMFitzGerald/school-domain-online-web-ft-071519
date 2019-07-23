b0949852d65cc9b30eed682a83c1c52fe5b9bece
Emma FitzGerald <emmamariafitzgerald@gmail.com>
2019-07-23 15:54:51 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,14 +1,15 @@
 require "pry"
 class School
+  attr_accessor :roster
     
   def initialize(school_name)
     @school_name = school_name
     @roster = {}
   end
   
-  def roster
-    @roster = {}
-  end
+  # def roster
+  #   @roster = {}
+  # end
   
   def add_student(name, grade)
     unless @roster.include?(grade)
