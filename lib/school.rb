a2e4afd1d32a3c395bd0d6cd35bb16aed714fe45
Emma FitzGerald <emmamariafitzgerald@gmail.com>
2019-07-23 15:52:42 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -3,11 +3,12 @@ class School
 
   def initialize(school_name)
     @school_name = school_name
-  end
-  
-  def roster
     @roster = {}
   end
+  
+  # def roster
+  #   @roster = {}
+  # end
   
   def add_student(name, grade)
     unless @roster.include?(grade)
