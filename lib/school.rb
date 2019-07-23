4c96e244bbc154ca9b2162d57fa5aa989c872e18
Emma FitzGerald <emmamariafitzgerald@gmail.com>
2019-07-23 15:39:11 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,19 @@
-# code here!
+require "pry"
+binding.pry
+class School
+
+  def initialize(school_name)
+    @school_name = school_name
+  end
+  
+  def roster
+    @roster = {}
+  end
+  
+  def add_student(name, grade)
+    @roster[grade] = []
+    @roster[grade] << name
+    binding.pry
+  end
+  
+end
